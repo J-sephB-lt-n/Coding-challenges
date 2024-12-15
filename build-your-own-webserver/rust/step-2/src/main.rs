c1f34f3c06b2da4877a3862b77d08e1f9055b0d8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-12-15 19:58:27 +0200

    feat(src/main.rs): returns 200 if file found else 404

--- build-your-own-webserver/rust/step-2/src/main.rs
+++ build-your-own-webserver/rust/step-2/src/main.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
 use std::{
+    fs::File,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    path::Path,
 };
 
 fn main() {
@@ -25,21 +27,44 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     let first_line_http_request: &str = &http_request[0];
-    let key_params_regex = Regex::new(r"^([A-Z]+)\s\/([\w\/]*)\s(.+)$").unwrap();
-    let request_captures = key_params_regex.captures(first_line_http_request).unwrap();
+    let key_params_regex = Regex::new(r"^([A-Z]+)\s\/(.*)\s(.+)$").unwrap();
+    let request_captures = match key_params_regex.captures(first_line_http_request) {
+        Some(captures) => captures,
+        None => {
+            panic!("Error parsing HTTP request");
+        }
+    };
     // let request_method = request_captures.get(1).unwrap().as_str();
     let request_path = request_captures.get(2).unwrap().as_str();
+    let request_path = if request_path.is_empty() {
+        "index.html"
+    } else {
+        request_path
+    };
+    let request_path = format!("www/{request_path}");
     // let request_protocol = request_captures.get(3).unwrap().as_str();
 
-    println!("Request: {http_request:#?}");
+    // println!("Request: {http_request:#?}");
 
-    let response_status_line = "HTTP/1.1 200 OK";
+    let file_path = Path::new(&request_path);
+    println!("{file_path:#?}");
+
+    // let response_status_line = "HTTP/1.1 200 OK";
+    let response_status_line = if file_path.exists() && file_path.is_file() {
+        "HTTP/1.1 200 OK"
+    } else {
+        "HTTP/1.1 404 Not Found"
+    };
     let response_contents = format!("Requested path: {request_path}");
     let response_len = response_contents.len();
+    let http_response = format!(
+        "{response_status_line}\r\nContent-Length: {response_len}\r\n\r\n{response_contents}"
+    );
 
-     let http_response = format!(
-         "{response_status_line}\r\nContent-Length: {response_len}\r\n\r\n{response_contents}"
-     );
 
-     stream.write_all(http_response.as_bytes()).unwrap();
+
+    match stream.write_all(http_response.as_bytes()) {
+        Ok(()) => println!("Response sent successfully"),
+        Err(e) => eprintln!("Failed to send response: {}", e),
+    }
 }
